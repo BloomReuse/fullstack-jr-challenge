8f3f3e19e4f8815d7465f56495c1cf7b4fdd7855
Florencia Valladares <fvalladares1@uc.cl>
2025-05-06 23:15:15 -0400

    feat(backend): add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
-import express from "express";
-import routes from "./api";
+import express from 'express';
+import routes from './api';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use("/", routes);
+app.use('/', routes);
 
 app.listen(8000, () => {
   console.log(`Server running on port 8000`);
