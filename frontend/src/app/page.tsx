4c322d1d152f3365a693a04a9c88a327721b91ea
Florencia Valladares <fvalladares1@uc.cl>
2025-05-06 21:49:19 -0400

    feat(frontend): add next config

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Home from "@/modules/home";
+
+export default function Page() {
+  return (
+    <main>
+      <Home />
+    </main>
+  );
+}
---
+++ frontend/src/modules/home/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Desafío Dev Bloom Reuse 2025</h1>
+    </div>
+  );
+}
